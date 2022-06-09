2b41ffbb1ce87f0aad28a48e870634c738e0d92b
Kenneth Albert <gichuka@gmail.com>
2022-06-09 22:24:38 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+   return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office"){
+   return  `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(visual="*"){
+    const adjective=function string(message){
+       return  `You are ${visual}${message}${visual}!`
+    }
+    return adjective;
+}
